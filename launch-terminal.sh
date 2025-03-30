530e3af0f542feb7dbec13a3b4eabb6a08381c3f
Sven Graupner <sgraupner@bht-berlin.de>
2025-03-30 21:11:11 +0200

    update settings.json, launch.json, launch_terminal.sh

--- launch-terminal.sh
+++ launch-terminal.sh
@@ -4,15 +4,11 @@
 # 
 # probe java is installed
 if [[ $(type java &>/dev/null; echo $?) == 0 ]]; then
+    # 
     # source $HOME/.bashrc or $HOME/.zshrc (Mac)
     [[ "$SHELL" =~ ^.*zsh$ ]] && \
         source ${HOME}/.zshrc || source ${HOME}/.bashrc
     # 
-    # on Windows, switch code page to UTF-8 (Windows only)
-    [[ $(type chcp.com &>/dev/null; echo $?) == 0 ]] && \
-        chcp.com 65001 >/dev/null
-    # 
-    # source project, if unsoured ('setup()' function exists)
-    [[ -z "$(type setup 2>/dev/null | grep function)" ]] && \
-        [[ -f .env.sh ]] && source .env.sh
+    # source project when new terminal is opened
+    [[ -f .env/env.sh ]] && source .env/env.sh
 fi

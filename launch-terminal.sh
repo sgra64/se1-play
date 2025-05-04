931a0b955154071f7b7b7fccb1ec20bbc600066d
Sven Graupner <sgraupner@bht-berlin.de>
2025-05-04 02:10:03 +0200

    update launch-terminal.sh to source .bashrc and env.sh

--- launch-terminal.sh
+++ launch-terminal.sh
@@ -2,13 +2,14 @@
 # initialize new terminal (bash, zsh/Mac), script is called in settings.json
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # 
-# probe java is installed
-if [[ $(type java &>/dev/null; echo $?) == 0 ]]; then
-    # 
-    # source $HOME/.bashrc or $HOME/.zshrc (Mac)
-    [[ "$SHELL" =~ ^.*zsh$ ]] && \
-        source ${HOME}/.zshrc || source ${HOME}/.bashrc
-    # 
-    # source project when new terminal is opened
-    [[ -f .env/env.sh ]] && source .env/env.sh
-fi
+# probe whether terminal is opened on Mac with zsh and
+# source $HOME/.zshrc for zsh (Mac) or $HOME/.bashrc otherwise
+type setopt 2>/dev/null | grep builtin >/dev/null
+[ $? = 0 ] && \
+    source ~/.zshrc ||
+    source ~/.bashrc
+# 
+# source the project when a new terminal is opened in VSCode
+for env in "env.sh" ".env/env.sh"; do
+    [ -f "$env" ] && source "$env" && break
+done

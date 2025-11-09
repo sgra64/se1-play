a0df5e0be2ea7875e7514f5884d6ea3061318e1c
Sven Graupner <sgraupner@bht-berlin.de>
2025-11-09 01:27:26 +0100

    add src/main, src/main/numbers

---
+++ src/main/application/Application.java
@@ -0,0 +1,31 @@
+package application;
+
+import numbers.Numbers;
+
+/**
+ * Application class with a {@code main()} - function that parses command line
+ * arguments.
+ * 
+ * @version <code style=color:green>{@value application.package_info#Version}</code>
+ * @author <code style=color:blue>{@value application.package_info#Author}</code>
+ */
+public class Application {
+
+    /**
+     * {@code main()} - function as entry point for the Java VM.
+     * @param args arguments passed from the command line
+     */
+    public static void main(String[] args) {
+        var module = Application.class.getModule().getName();
+        var greeting = String.format(module==null? "%s, se1-play" : "%s, %s (modular)", "Hello", module);
+        System.out.println(greeting);
+
+        // java.util.Arrays.stream(args)
+        //     .map(arg -> String.format(" - arg: %s", arg))
+        //     .forEach(System.out::println);
+        // 
+        Numbers numbers = Numbers.getInstance();
+        Runner runner = Numbers.createRunner(numbers);
+        runner.run(args);
+    }
+}

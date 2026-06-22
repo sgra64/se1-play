c34dbc5814cbde5c5c5ec524ff34847aa034c137
Sven Graupner <sgraupner@bht-berlin.de>
2026-06-22 09:48:30 +0200

    add javadoc ordering-system

---
+++ ordering-system/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"application"},{"l":"components"},{"l":"components.impl"},{"l":"datamodel"},{"l":"patterns"}];updateSearchResults();
